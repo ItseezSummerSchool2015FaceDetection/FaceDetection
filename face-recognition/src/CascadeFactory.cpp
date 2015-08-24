e5f7566e4d72ecd43b3c4d8f0bd54ca15e17c126
xamcaa <ss2015@ws-2k-110-n09.ccam.cmc.unn.net>
2015-08-24 18:20:40 +0300

    factory added

---
+++ face-recognition/src/CascadeFactory.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+class CascadeFactory
+{
+
+}
+int main()
+{
+    cv::CascadeClassifier
+	return 0;
+}
